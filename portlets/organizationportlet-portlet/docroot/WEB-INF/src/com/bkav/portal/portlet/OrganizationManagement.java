c798e517b4421e2d861d6666558433f2feaad370
quangcv1206 <chuvanquang96@gmail.com>
2019-02-25 23:53:23 +0700

    Hiazz

---
+++ portlets/organizationportlet-portlet/docroot/WEB-INF/src/com/bkav/portal/portlet/OrganizationManagement.java
@@ -0,0 +1,11 @@
+package com.bkav.portal.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class OrganizationManagement
+ */
+public class OrganizationManagement extends MVCPortlet {
+ 
+
+}
---
+++ portlets/organizationportlet-portlet/docroot/WEB-INF/src/com/bkav/portal/portlet/organization/OrganizationPortlet.java
@@ -0,0 +1,11 @@
+package com.bkav.portal.portlet.organization;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class OrganizationPortlet
+ */
+public class OrganizationPortlet extends MVCPortlet {
+ 
+
+}
